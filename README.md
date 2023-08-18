763f89ee6d92eb3aaaffd2dd89a03ac5cfcd22a0
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-08-18 22:09:17 +0500

    minor updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Advanced Node and Express
 
-This is the boilerplate for the Advanced Node and Express lessons. Instructions for the lessons start at https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/
+A simple app to connect via sockets and use PassportJS to authorize users.
--- server.js
+++ server.js
@@ -1,35 +1,105 @@
 'use strict';
 require('dotenv').config();
-const createError = require('http-errors')
 const express = require('express');
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session')
+const passport = require('passport')
+const routes = require('./routes.js')
+const auth = require('./auth.js');
+const passportSocketIo = require('passport.socketio')
+const MongoStore = require('connect-mongo')(session)
+const cookieParser = require('cookie-parser')
+
+const URI = process.env.MONGO_URI
+const store = new MongoStore({
+  url: URI
+})
 
 const app = express();
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: store,
+  key: 'express.sid',
+  cookie: { secure: false }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 fccTesting(app); //For FCC testing purposes
+app.use(logger("dev"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.set('view engine', 'pug')
 app.set('views', './views/pug')
 
-app.route('/').get((req, res) => {
-  res.render("index")
-});
+io.use(passportSocketIo.authorize({
+  cookieParser: cookieParser,
+  key: 'express.sid',
+  secret: process.env.SESSION_SECRET,
+  store: store,
+  success: onAuthorizeSuccess,
+  fail: onAuthorizeFail
+}))
 
-app.use((req, res, next) => {
-  next(createError(404))
+myDB(async client => {
+  const myDatabase = await client.db('database').collection('users')
+  let currentUsers = 0
+  io.on('connection', socket => {
+    console.log('A user has connected')
+    ++currentUsers
+    // emit event on connect
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    })
+    // listen to the socket for the event
+    socket.on('chat message', (message) => {
+      io.emit('chat message', {
+        username: socket.request.user.username,
+        message
+      })
+    })
+    socket.on('disconnect', () => {
+      --currentUsers
+      // emit event on disconnect
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      })
+    })
+  })
+  routes(app, myDatabase)
+  auth(app, myDatabase)
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' })
+  })
 })
-app.use((err, req, res, next) => {
-  res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
-  res.status(err.status || 500)
-  res.render("error")
-})
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io')
+  accept(null, true)
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message)
+  console.log('failed connection to socket.io: ', message)
+  accept(null, false)
+}
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
